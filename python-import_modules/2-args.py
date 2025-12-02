d727bda7ce39291fd008f8c4a9caad0d43f51f12
Khalid <khalidalomari199@gmail.com>
2025-12-02 10:53:14 +0300

    correct tha blank order

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     lenarg = len(sys.argv)
     arg = sys.argv
     i = 2
-    if len(sys.argv) <= 1:
-        print("{} argument.".format(0))
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(0))
     elif len(sys.argv) == 2:
         print("{} argument:".format(lenarg - 1))
         print("{}: {}".format(1, arg[1]))
