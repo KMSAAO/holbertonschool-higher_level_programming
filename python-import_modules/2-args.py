339ded31b7e597569227155e1c9c62272f9ddf5b
Khalid <khalidalomari199@gmail.com>
2025-12-02 10:51:08 +0300

    correct the blank order

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     lenarg = len(sys.argv)
     arg = sys.argv
     i = 2
-    if len(sys.argv) == 1:
+    if len(sys.argv) <= 1:
         print("{} argument.".format(0))
     elif len(sys.argv) == 2:
         print("{} argument:".format(lenarg - 1))
