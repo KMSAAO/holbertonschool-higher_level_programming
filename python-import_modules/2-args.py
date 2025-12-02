6f5d9e898ddd5411c0d3d8f6cd118824da0841f6
Khalid <khalidalomari199@gmail.com>
2025-12-02 10:49:52 +0300

    correct file

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,7 +6,10 @@ if __name__ == "__main__":
     i = 2
     if len(sys.argv) == 1:
         print("{} argument.".format(0))
-    else:
+    elif len(sys.argv) == 2:
         print("{} argument:".format(lenarg - 1))
+        print("{}: {}".format(1, arg[1]))
+    else:
+        print("{} arguments:".format(lenarg - 1))
         for i in range(1, lenarg):
             print("{}: {}".format(i, arg[i]))
