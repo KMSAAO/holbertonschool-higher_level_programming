3453a4f3876c7d1cb0024e1ca06b52af18cba8a7
Khalid <khalidalomari199@gmail.com>
2025-12-02 09:45:16 +0300

    add correct answer

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+
 def add(a, b):
-    if __name__ == "__import__":
-        return (0)
+    return a + b
+
+
+if __name__ == "__main__":
     import add_0
+    a = 1
+    b = 2
     print("{} + {} = {}".format(a, b, add_0.add(a, b)))
