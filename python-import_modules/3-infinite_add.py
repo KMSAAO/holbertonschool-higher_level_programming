c85f6a735dfd3ba7a6d1294bda668361c2bbd869
Khalid <khalidalomari199@gmail.com>
2025-12-02 11:20:52 +0300

    add file

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-arg = sys.argv
-a = 0
-for i in range(1, len(arg)):
-    a += int(arg[i])
+    arg = sys.argv
+    a = 0
+    for i in range(1, len(arg)):
+        a += int(arg[i])
 
-print(a, end="\n")
+    print(a, end="\n")
