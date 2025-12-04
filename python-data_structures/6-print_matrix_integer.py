bfc66f5be0a8e9b64bfcbfddf3886a423e07cd33
Khalid <khalidalomari199@gmail.com>
2025-12-04 18:03:04 +0300

    fix the newline

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     b = 1
-    for i in range(len(matrix)):
-        if i == b:
-            print(end="\n")
-            b += 1
-        for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=" ")
-    print("", end="\n")
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            print("{:d} ".format(matrix[row][col]), end=" ")    
+        print("", end="\n")
