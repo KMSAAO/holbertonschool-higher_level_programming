d832878ad053d662090dcdef113f86bc3a183640
Khalid alomari <khalidalomari199@gmail.com>
2025-12-04 23:24:49 +0300

    add add_tuple function

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if not tuple_a:
+        tuple_a = list(tuple_a)
+        tuple_a.extend([0, 0])
+        tuple_a = tuple(tuple_a)
+    elif not tuple_b:
+        tuple_b = list(tuple_b)
+        tuple_b.extend([0, 0])
+        tuple_b = tuple(tuple_b)
+    elif len(tuple_a) < 2:
+        tuple_a = list(tuple_a)
+        tuple_a.append(0)
+        tuple_a = tuple(tuple_a)
+    elif len(tuple_b) < 2:
+        tuple_b = list(tuple_b)
+        tuple_b.append(0)
+        tuple_b = tuple(tuple_b)
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return (result)
