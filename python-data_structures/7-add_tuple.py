fcc46cbb3904f36ce5ae810e5359e99a37b83d2b
Khalid alomari <khalidalomari199@gmail.com>
2025-12-04 23:42:20 +0300

    fix erorr when the tuple less than 2

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -17,5 +17,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_b = list(tuple_b)
         tuple_b.append(0)
         tuple_b = tuple(tuple_b)
+
+    if not tuple_a and tuple_b:
+        new_tupel = (0, 0)
+        return (new_tupel)
     result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return (result)
