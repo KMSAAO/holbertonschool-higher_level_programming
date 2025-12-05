91cff5d4e5586cf17c87be714b553b394e2b2cd1
Khalid <khalidalomari199@gmail.com>
2025-12-06 01:35:52 +0300

    fix if there any elements duplecated will remove it

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -2,4 +2,7 @@
 def only_diff_elements(set_1, set_2):
     lis = list(set_1)
     lis.extend(set_2)
-    return set(lis)
+    for x in lis:
+        if x in set_2 and x in set_1:
+            lis.remove(x)
+    return lis
