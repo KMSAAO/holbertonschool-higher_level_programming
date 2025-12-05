899bf8e95296396f962deb9aacb99fcdfa68b010
Khalid <khalidalomari199@gmail.com>
2025-12-06 01:14:43 +0300

    fix if there two duplcated elements

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    lis = []
     for x in set_1:
         if x in set_2:
-            lis = [x]
-        else:
-            lis = []
+            lis.append(x)
     return lis
