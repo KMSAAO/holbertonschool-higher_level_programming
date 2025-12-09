5aa3f76b8b46337ca94350463922a6fb7d44486a
Khalid <khalidalomari199@gmail.com>
2025-12-09 22:23:58 +0300

    add square file

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a Square class"""
+
+
+class Square:
+    """Represents a Square"""
+
+    def __init__(self, size):
+        """Initializes a new square
+
+        Args:
+            size (int): The size of square
+        """
+        self.__size = size
