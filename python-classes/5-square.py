a139ba184cbfba80157d5b6c0e9a760b87bf1e30
Khalid <khalidalomari199@gmail.com>
2025-12-10 17:09:19 +0300

    add my_print method

---
+++ python-classes/5-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Define a square class"""
+
+
+class Square:
+    """
+    Represents a Square class
+    """
+    def __init__(self, size=0):
+        """
+        Insitializes a new Square
+
+        :param size (int): its the size of the square, and the Default is 0
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Insitializes size
+
+        :param value: is the value of __size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """area is square any number"""
+        square1 = self.__size * self.__size
+        return square1
+
+    def my_print(self):
+        """my_print will print in stdout the square with the charecter (#) """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
