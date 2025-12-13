6f96a063645360e445055735cd609976d696dcc2
Khalid <khalidalomari199@gmail.com>
2025-12-13 18:48:36 +0300

    edit the condition

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
     """Create a new class Rectangle"""
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -24,7 +24,7 @@ class Rectangle:
         """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value <= 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -41,6 +41,6 @@ class Rectangle:
         """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value <= 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
