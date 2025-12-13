79467e2ada41d21c673a6c476d068cf64c75c4fb
Khalid <khalidalomari199@gmail.com>
2025-12-13 20:11:59 +0300

    add a static method begger_or_equal

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -81,7 +81,7 @@ class Rectangle:
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         elif not isinstance(rect_2, Rectangle):
-            raise TabError("rect_2 must be an instance of Rectangle")
+            raise TypeError("rect_2 must be an instance of Rectangle")
         elif Rectangle.area(rect_1) == Rectangle.area(rect_2):
             return rect_1
         elif Rectangle.area(rect_1) > Rectangle.area(rect_2):
