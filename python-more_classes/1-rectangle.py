dc5a3db7b187f1dfc378b2172b953303ca067e30
Khalid <khalidalomari199@gmail.com>
2025-12-13 18:44:37 +0300

    edit the condition

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -24,7 +24,7 @@ class Rectangle:
         """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        elif value <= 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -41,6 +41,6 @@ class Rectangle:
         """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        elif value <= 0:
             raise ValueError("height must be >= 0")
         self.__height = value
