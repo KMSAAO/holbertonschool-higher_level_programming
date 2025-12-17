a24840608e80d4ed3ea1c63200c921fb75f9aee8
Khalid alomari <khalidalomari199@gmail.com>
2025-12-18 01:49:29 +0300

    add doc for mylist

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,7 +3,11 @@
 class MyList that inherits from list python-inheritance.1-my_list
 """
 
+
 class MyList(list):
+    """
+    class MyList that inherits from list
+    """
     def print_sorted(self):
         """
         This method will be print the list, but sorted ascending sort
