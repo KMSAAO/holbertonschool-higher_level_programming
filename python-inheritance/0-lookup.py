574092396856cdba01c43eda2fa2091f58f82d93
Khalid alomari <khalidalomari199@gmail.com>
2025-12-18 00:42:51 +0300

    add function doc

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -6,4 +6,9 @@ python-inheritance.0-lookup
 
 
 def lookup(obj):
+    """
+    This function will return the list of methods an object
+
+    :param obj: this is the object will return the list of methods
+    """
     return dir(obj)
