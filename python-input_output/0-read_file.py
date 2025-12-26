b266f82b821489fc0c7d3fe4467e5cfb112545e9
Khalid <khalidalomari199@gmail.com>
2025-12-26 16:04:57 +0300

    add read_file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python
+"""
+This file include read_file function
+"""
+
+
+def read_file(filename=""):
+    """
+    This function will read the file and prints it
+    
+    :param filename: The name of the file will read
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        content = file.read()
+        print(content, end="")
