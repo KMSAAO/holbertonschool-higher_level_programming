cb700d5933ec684415f453e7cb8fff8e540b1f01
Khalid <khalidalomari199@gmail.com>
2025-12-26 16:05:44 +0300

    fix pycodestyle errors

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,7 +7,7 @@ This file include read_file function
 def read_file(filename=""):
     """
     This function will read the file and prints it
-    
+
     :param filename: The name of the file will read
     """
     with open(filename, "r", encoding="utf-8") as file:
