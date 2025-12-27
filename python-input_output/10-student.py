45ad191acd644ed50d112f754ba7909f7c3c07d2
Khalid alomari <khalidalomari199@gmail.com>
2025-12-27 20:36:21 +0300

    add attrs to to_json method

---
+++ python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This file include class Student
+"""
+
+
+class Student():
+    """
+    This is a class for Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(i, str) for i in attrs):
+            new_dict = {}
+            for j in attrs:
+                if j in self.__dict__:
+                    new_dict[j] = self.__dict__[j]
+            return new_dict
+        else:
+            return self.__dict__
