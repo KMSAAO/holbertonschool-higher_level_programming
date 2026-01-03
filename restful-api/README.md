b1b0aaaaca3593bc7285fd716e28cbe78eb5dcee
Khalid alomari <khalidalomari199@gmail.com>
2026-01-03 16:14:59 +0300

    add file

---
+++ restful-api/README.md
@@ -0,0 +1,1 @@
+i will learn restful api
---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+users = {}
+
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+
+
+@app.route("/data")
+def get_username():
+    return jsonify(list(users.keys()))
+
+
+@app.route("/users/<username>")
+def get_user(username):
+    user = users.get(username)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+
+@app.route("/status")
+def status():
+    return "OK"
+
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    data = request.get_json(silent=True)
+    if not data or not isinstance(data, dict) or "username" not in data:
+        return jsonify({"error": "Username is required"}), 400
+
+    username = data["username"]
+    if username in users:
+        return jsonify({"error": "Username already exists"}), 409
+
+    users[username] = data
+    return jsonify({"message": "User added", "user": data}), 201
+
+
+if __name__ == "__main__":
+    app.run()
