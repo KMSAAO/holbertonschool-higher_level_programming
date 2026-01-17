4ebd7e6eca5f832a42c520a7fecf375b49468eff
Khalid alomari <Khalidalomari199@gmail.com>
2026-01-17 22:12:33 +0300

    fix the error

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,15 +4,16 @@ import sys
 """
     Script that lists all states
 """
-db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-cur = db.cursor()
+    cur = db.cursor()
 
-cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states")
 
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
