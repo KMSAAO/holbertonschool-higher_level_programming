7fe1b2ce8a4a23d76e65915f6a67fc124535be5b
Khalid alomari <khalidalomari199@gmail.com>
2026-02-07 20:28:01 +0300

    add files

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,19 @@
+import json
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/items')
+def items():
+    try:
+        with open('items.json', 'r') as f:
+            data = json.load(f)
+            items_list = data.get("items", [])
+    except (FilePathError, FileNotFoundError):
+        items_list = []
+
+    return render_template('items.html', items=items_list)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
